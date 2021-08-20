8fedbd76f9cbde628dc9c41b1852608cef768f2a
Tito Garrido Ogando <titogarrido@gmail.com>
2021-08-20 16:14:02 -0300

    Update Temp.vue

--- src/components/Temp.vue
+++ src/components/Temp.vue
@@ -17,22 +17,12 @@
 
         <v-row justify="center">
           <v-card flat>
-            <iframe
-              title="Endereço para atendimento"
-              width="400"
-              height="300"
-              id="gmap_canvas"
-              src="https://maps.google.com/maps?q=R.+Borges+Lagoa,+1080+-+Vila+Clementino,+São+Paulo+-+SP,+04038-002&t=&z=17&ie=UTF8&iwloc=&output=embed"
-              frameborder="0"
-              scrolling="no"
-              marginheight="0"
-              marginwidth="0"
-            ></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1984227810517!2d-46.641267684629405!3d-23.561316067476636!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce59a5a99f7cb7%3A0x887a5a5724586542!2sAv.%20da%20Liberdade%2C%201000%20-%20Liberdade%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2001502-001!5e0!3m2!1spt-BR!2sbr!4v1629486319828!5m2!1spt-BR!2sbr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
             <v-card-title primary-title>
               <div>
                 <h5 class="subtitle-1 font-weight-thin">
-                  Rua Borges Lagoa 1080, Sala 1210 - Vila Clementino - São Paulo
+                  Avenida Liberdade 1000, Sala 1413 - Liberdade - São Paulo
                 </h5>
               </div>
             </v-card-title>
